cafc6bdfcb88599077af8b2262bb9e8b047a79c9
kenjitaketomi <13311445+kenjitaketomi@users.noreply.github.com>
2021-08-12 21:35:01 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -61,3 +61,16 @@ document.addEventListener('DOMContentLoaded', function() {
     MenuEle.addEventListener('click', navToggle);
     // $wrapper.addEventListener('click',navToggle);
   });
+
+
+// スムーススクロール(aタグのリンクに#がつく場合)
+$(function(){
+  $('a[href^="#"]').click(function(){
+    var speed = 500;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+});
